f0233e43168a2a6f4fc5d185d9d156158a56d9f3
mananjabrik <manan.jabrik@gmail.com>
2023-10-08 16:44:21 +0700

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";	
   };
@@ -11,14 +10,13 @@
   let 
     inherit (self) outputs; 
   in {
-
     nixosConfigurations = {
-	os = nixpkgs.lib.nixosSystem {
-	   specialArgs = {inherit inputs outputs;};
-	   modules = [
-	     ./nixos/configuration.nix
-	   ];
-	};
+      os = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
     };
   };
 }
