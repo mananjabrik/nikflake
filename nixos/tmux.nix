a685703734fe27df31db263cd27dac232b5265f4
mananjabrik <manan.jabrik@gmail.com>
2023-10-08 13:14:10 +0700

    setup temux

--- nixos/tmux.nix
+++ nixos/tmux.nix
@@ -3,9 +3,10 @@
     programs.tmux = {
     enable = true;
     clock24 = true;
-    extraConfig = ''
-     
-    '';
+    extraConfig = '' # used for less common options, intelligently combines if defined in multiple places.
+     set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
+     run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+  '';
   };
 }
 
